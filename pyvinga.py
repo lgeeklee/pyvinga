fef4f87e33ec148a8b23f7586c4e4ee725e480fe
lgeeklee <leeb78@gmail.com>
2015-03-11 20:25:02 +0000

    Added Host CPU

--- pyvinga.py
+++ pyvinga.py
@@ -120,7 +120,7 @@ def host_cpu_usage(host_moref, warning, critical):
     :param host_moref: Managed Object Reference for the ESXi Host
     """
     host_total_cpu = host_moref.summary.hardware.cpuMhz * host_moref.summary.hardware.numCpuCores
-    host_cpu = host_moref.summary.quickstats.overallCpuUsage
+    host_cpu = host_moref.summary.quickStats.overallCpuUsage
 
     final_output = (host_cpu /host_total_cpu) * 100
 
